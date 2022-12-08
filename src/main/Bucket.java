b354a1e80985c1ecacd3cf850ff446df3a663a71
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-12-08 11:35:44 -0500

    method created to put the words into an array

--- src/main/Bucket.java
+++ src/main/Bucket.java
@@ -1,4 +1,29 @@
 package main;
 
 public class Bucket {
+
+    private class Node {
+
+        public String word;
+        public Node next;
+
+        public Node(String word, Node next) {
+            this.word = word;
+            this.next = next;
+        }
+    }
+
+    private Node first;
+
+//    Method to put the words in the dictionary
+    public void put(String key) {
+        for(Node curr = first; curr != null; curr = curr.next) {
+            if(key.equals(curr.word)) {     // If word is a duplicate, already exists in the dictionary: return
+                return;
+            }
+        }
+        first = new Node(key, first);       // Add that word in the dictionary
+    }
+
+
 }
