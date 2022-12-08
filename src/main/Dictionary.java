df322b0b70b4a4189a91b04edc6b693098680605
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-12-08 12:50:59 -0500

    add method added in dictionary which uses bucket array and hash to put the word at index generated by the hash method and places that in the array

--- src/main/Dictionary.java
+++ src/main/Dictionary.java
@@ -1,4 +1,26 @@
 package main;
 
 public class Dictionary {
+
+    private int num = 1319; // Prime Number
+    private Bucket[] bucketArray;
+
+    private Dictionary() {
+
+        bucketArray = new Bucket[num];
+
+        for(int i = 0; i < num; i++) {
+            bucketArray[i] = new Bucket();
+        }
+    }
+
+//    Hash
+    private int hash(String key) {
+        return (key.hashCode() & 0x7fffffff) % num;
+    }
+
+//    add
+    public void add(String key) {
+        bucketArray[hash(key)].put(key);
+    }
 }
