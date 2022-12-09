9d2582784828df2b66e598ddfdb159ab7ba49974
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-12-09 15:27:11 -0500

    seperate class created to run checkWordsInFile

---
+++ src/main/CheckWordsInFile.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.io.IOException;
+
+public class CheckWordsInFile {
+
+    public static void main(String[] args) throws IOException {
+
+        SpellChecker spellChecker = new SpellChecker();
+        spellChecker.checksWordsInFile();
+    }
+}
