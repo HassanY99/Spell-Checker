476cd8217d9b0036612f869d25cc3cc7c98d0f2b
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-12-09 15:26:08 -0500

    seperate class created to run checkSenetence()

---
+++ src/main/ChecksSentence.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.io.IOException;
+
+public class ChecksSentence {
+
+    public static void main(String[] args) throws IOException {
+        SpellChecker spellChecker = new SpellChecker();
+        spellChecker.checksSentence();
+    }
+}
